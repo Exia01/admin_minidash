a5e8d72c836af242f308631199f5731dd755a741
Jose <gonzalezj@protonmail.com>
2022-06-16 13:30:12 -0400

    Update custom-error.js

--- server/utils/errors/custom-error.js
+++ server/utils/errors/custom-error.js
@@ -1,4 +1,6 @@
 //exported in case we want to use it
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Classes_in_JavaScript
+// https://futurestud.io/tutorials/node-js-create-your-custom-error
 class CustomAPIError extends Error {
   constructor(message, statusCode) {
     super(message);
