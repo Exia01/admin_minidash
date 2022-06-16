00c453aa55cbb50b777cce1352e4137397a2eefe
Jose <gonzalezj@protonmail.com>
2022-06-16 13:31:26 -0400

    remove not -found import

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -2,12 +2,10 @@ import express from 'express';
 
 import messageRouter from './message.js';
 import productRouter from './product.js';
-import { notFound } from './../../middleware/not-found.js';
 
 const router = express.Router();
 
 router.use('/message', messageRouter);
 router.use('/product', productRouter)
-// router.use(notFound)
 
 export default router;
