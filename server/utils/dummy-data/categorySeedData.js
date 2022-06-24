91fdbbe6335f94a8383644f5c4d4e07cda9d1aee
Jose <gonzalezj@protonmail.com>
2022-06-24 11:28:04 -0400

    Create categorySeedData.js

---
+++ server/utils/dummy-data/categorySeedData.js
@@ -0,0 +1,14 @@
+// products
+// Data from: https://fakestoreapi.com/products#
+
+export const categoryData = [
+  {
+    name: 'jewelery',
+  },
+  {
+    name: "men's clothing",
+  },
+  {
+    name: 'electronics',
+  },
+];
