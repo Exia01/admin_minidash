395a4337327f9404980946de7dd65a410b54d8ce
Jose <gonzalezj@protonmail.com>
2022-06-30 12:43:05 -0400

    Update index.js

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 
 import messageRouter from './message.js';
 import productRouter from './product.js';
+import categoryRouter from './category.js';
 
 const router = express.Router();
 
 router.use('/message', messageRouter);
-router.use('/product', productRouter)
+router.use('/product', productRouter);
+router.use('/category', categoryRouter);
 
 export default router;
