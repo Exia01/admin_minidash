48098ab889bb4dbb23e193786e86c5430554759c
Jose <gonzalezj@protonmail.com>
2022-07-07 16:37:13 -0400

    Add shop routes

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import messageRouter from './message.js';
 import productRouter from './product.js';
 import categoryRouter from './category.js';
+import shopRouter from './shop.js';
 
 const router = express.Router();
 
 router.use('/message', messageRouter);
 router.use('/product', productRouter);
 router.use('/category', categoryRouter);
+router.use('/shop', shopRouter);
 
 export default router;
