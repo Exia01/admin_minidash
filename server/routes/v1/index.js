8333aebe225f4120605870dbf3179dd3d8281a17
Jose <gonzalezj@protonmail.com>
2022-07-14 10:10:12 -0400

    Update index.js

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -4,6 +4,7 @@ import messageRouter from './message.js';
 import productRouter from './product.js';
 import categoryRouter from './category.js';
 import shopRouter from './shop.js';
+import saleEntryRouter from './saleEntry.js';
 
 const router = express.Router();
 
@@ -11,5 +12,7 @@ router.use('/message', messageRouter);
 router.use('/product', productRouter);
 router.use('/category', categoryRouter);
 router.use('/shop', shopRouter);
+router.use('/sale-entry', saleEntryRouter);
+// router.use('/shop', shopRouter);
 
 export default router;
