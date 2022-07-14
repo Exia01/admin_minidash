abea6418e3b46f3578cb00243a9d61ff32a83ab9
Jose <gonzalezj@protonmail.com>
2022-07-14 09:55:25 -0400

    Create SaleEntry model

---
+++ server/models/SaleEntry.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose';
+// const Schema = mongoose.Schema;
+const { Schema } = mongoose;
+
+const SaleEntrySchema = new Schema(
+  {
+    shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
+    total: {
+      type: Number,
+      required: [true, 'Please provide total for sale entry'],
+    },
+    yearPeriod: {
+      type: String,
+      required: [true, 'Please provide a year figure'],
+    },
+    quarter: {
+      type: String,
+      required: [true, 'Please provide a quarter period'],
+    },
+    month: {
+      type: String,
+      required: [true, 'Please provide a month  '],
+    },
+    schemaVer: {
+      type: Number,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+  // {
+  //   toJSON: { virtuals: true }, // So `res.json()` and other `JSON.stringify()` functions include virtuals
+  //   toObject: { virtuals: true }, // So `console.log()` and other functions that use `toObject()` include virtuals
+  // }
+);
+
+
+const SaleEntry = mongoose.model('SalesEntry', SaleEntrySchema);
+
+export default SaleEntry;
