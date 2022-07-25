12658bde35f161a36f0fa33987f54018e806af21
Jose <gonzalezj@protonmail.com>
2022-07-25 11:05:12 -0400

    Update reports.js

--- server/routes/v1/reports.js
+++ server/routes/v1/reports.js
@@ -1,9 +1,13 @@
-import Express from 'express';
+import express from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { buildYOYSalesReport } from '../../controllers/reports.js';
+const router = express.Router();
 
-const router = Express.router();
-
-router.get('/sales', buildSalesReport);
-router.get('/sales/shop/:shop_id', buildShopSalesReport);
-router.get('/products', buildProductsReport);
+// router.get('/sales', buildSalesReport);
+router.get('/sales/yoy/', buildYOYSalesReport);
+// router.get('/products', buildProductsReport);
+// router.get('/sales', buildSalesReport);
+// router.get('/sales/shop/:shop_id', buildShopSalesReport);
 // router.get('/sales', buildSalesReport)
+
+export default router;
