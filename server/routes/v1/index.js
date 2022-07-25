8d3554c7fec5f59032d121181a228f8ac5bf1ddd
Jose <gonzalezj@protonmail.com>
2022-07-25 11:05:26 -0400

    Add report route

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -5,6 +5,7 @@ import productRouter from './product.js';
 import categoryRouter from './category.js';
 import shopRouter from './shop.js';
 import saleEntryRouter from './saleEntry.js';
+import reportRouter from './reports.js';
 
 const router = express.Router();
 
@@ -13,6 +14,7 @@ router.use('/product', productRouter);
 router.use('/category', categoryRouter);
 router.use('/shop', shopRouter);
 router.use('/sale-entry', saleEntryRouter);
+router.use('/report', reportRouter);
 // router.use('/shop', shopRouter);
 
 export default router;
