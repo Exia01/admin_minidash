60eca74e3bc797857949822989800373e0d068fb
Jose <gonzalezj@protonmail.com>
2022-08-24 15:48:49 -0400

    Update auth.js

--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { registerUser, loginUser } from '../../controllers/auth.js';
+import { registerUser, loginUser, logoutUser } from '../../controllers/auth.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/logout', logoutUser);
 
 export default router;
