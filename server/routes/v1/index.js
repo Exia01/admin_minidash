cc32c54a8a5797f66162737fe60622ce90b24400
Jose <gonzalezj@protonmail.com>
2022-08-24 15:48:52 -0400

    Update index.js

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -7,16 +7,20 @@ import shopRouter from './shop.js';
 import saleEntryRouter from './saleEntry.js';
 import reportRouter from './reports.js';
 import authRouter from './auth.js';
+import userRouter from './user.js';
+import { authorization } from '../../middleware/authorization.js';
 
 const router = express.Router();
 
 router.use('/message', messageRouter);
+// router.use(authorization);
 router.use('/product', productRouter);
 router.use('/category', categoryRouter);
 router.use('/shop', shopRouter);
 router.use('/sale-entry', saleEntryRouter);
 router.use('/report', reportRouter);
 router.use('/auth', authRouter);
+router.use('/user', userRouter);
 // router.use('/shop', shopRouter);
 
 export default router;
