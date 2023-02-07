f2477320d88cef53ce8062ce424925e8359c75d1
Jose <gonzalezj@protonmail.com>
2023-02-07 16:23:58 -0500

    Add temp layout to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,58 @@
+import { Link } from 'react-router-dom';
 import './App.css';
+
+//MUI
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 
 function App() {
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
   return (
     <div className='App'>
-      <Button variant='contained'>Hello World</Button>
+      <Grid container spacing={2}>
+        <Grid xs={12} md={6}>
+          <Item>md=6</Item>
+        </Grid>
+        <Grid xs={12} md={6}>
+          <Typography variant='body2' component='p'>
+            <Item>md=6</Item>
+          </Typography>
+        </Grid>
+        <Grid xs={12} md={6}>
+          <Typography variant='body2' component='p'>
+            <Button
+              component={Link}
+              to='/login'
+              variant='contained'
+              color='primary'
+            >
+              Go to login page
+            </Button>
+          </Typography>
+        </Grid>
+        <Grid xs={12} md={6}>
+          <Typography variant='body2' component='p'>
+            <Button
+              component={Link}
+              to='/register'
+              variant='contained'
+              color='primary'
+            >
+              Go to register page
+            </Button>
+          </Typography>
+        </Grid>
+      </Grid>
     </div>
   );
 }
